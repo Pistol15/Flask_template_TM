6c0b6a39159081305d629b470663a18b0fb8c0cc
Pistol15 <143752578+Pistol15@users.noreply.github.com>
2024-01-15 10:59:37 +0100

    Authentification

--- app/views/auth.py
+++ app/views/auth.py
@@ -6,6 +6,10 @@ import os
 # Création d'un blueprint contenant les routes ayant le préfixe /auth/...
 auth_bp = Blueprint('auth', __name__, url_prefix='/auth')
 
+@auth_bp.route('/auth_auth', methods=('GET', 'POST'))
+def auth_auth():
+    return render_template("auth/auth_auth.html")
+
 # Route /auth/register
 @auth_bp.route('/register', methods=('GET', 'POST'))
 def register():
