d11a9cbac5909c4782c617777fa12fcd842bf6ca
Pistol15 <143752578+Pistol15@users.noreply.github.com>
2024-03-25 08:25:36 +0100

    Version rendu le 25.03.2024 à 8h25 Pas fini

--- app/views/prestation.py
+++ app/views/prestation.py
@@ -5,3 +5,23 @@ prestation_bp = Blueprint('prestation', __name__, url_prefix='/prestation')
 @prestation_bp.route('/prestation', methods=('GET', 'POST'))
 def prestation():
     return render_template("Prestation/prestation.html")
+
+@prestation_bp.route('/sport', methods=('GET', 'POST'))
+def Sport():
+    return render_template("Prestation/Sport.html")
+
+@prestation_bp.route('/anniversaire', methods=('GET', 'POST'))
+def anniversaire():
+    return render_template("Prestation/anniversaire.html")
+
+@prestation_bp.route('/animaux', methods=('GET', 'POST'))
+def animaux():
+    return render_template("Prestation/animaux.html")
+
+@prestation_bp.route('/portrait', methods=('GET', 'POST'))
+def portrait():
+    return render_template("Prestation/portrait.html")
+
+@prestation_bp.route('/concert', methods=('GET', 'POST'))
+def concert():
+    return render_template("Prestation/concert.html")
